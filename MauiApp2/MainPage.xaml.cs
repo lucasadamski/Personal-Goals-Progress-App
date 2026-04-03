d1faf85ea5657cff583d5daf8e31131550d3d4f0
Lucas A <lucasadams@protonmail.com>
2026-04-03 14:33:42 +0100

    Progress calculation logic

--- MauiApp2/MainPage.xaml.cs
+++ MauiApp2/MainPage.xaml.cs
@@ -26,10 +26,34 @@ namespace MauiApp2
         async void OnAddGoalClicked(object? sender, EventArgs eventArgs)
         {
             var button = (Button)sender;
-            await DisplayAlertAsync("Clicked", "Clicked the button add", "OK");
+            if(DateTime.TryParse(goalEndDate.Text, out DateTime validEndTimeOfAGoal))
+            {
+                await DisplayAlertAsync("Success", "Created new goal. Good luck!", "Close");
+                _db.CreateEntry(new Goal
+                {
+                    Title = goalName.Text,
+                    StartOn = DateTime.Now,
+                    EndOn = validEndTimeOfAGoal
+                });
+                Task.Run(async () => goalsCollection.ItemsSource = await _db.GetEntries());
+
+            }
+            else
+            {
+                await DisplayAlertAsync("Error", "Can't parse end date", "Close");
+            }
         }
 
-        
-
+        private async Task<double> CalculateGoalProgress(DateTime start, DateTime end)
+        {
+            var result = 0.0D;
+            if (start == null || end == null) return result;
+            if (DateTime.Today > end) return 1.0D;
+            if (start > end || DateTime.Today < start) return result;
+            var todayProgress = start - DateTime.Now;
+            var goalTime = end - start;
+            result = goalTime / todayProgress;
+            return result;
+        }
     }
 }
