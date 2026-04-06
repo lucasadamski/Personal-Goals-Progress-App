08f36c9b2fc78df9bc7a6dc056aa24926a9a9496
Lucas A <lucasadams@protonmail.com>
2026-04-06 07:49:45 +0100

    new test project

--- MauiApp2/Model/GoalDto.cs
+++ MauiApp2/Model/GoalDto.cs
@@ -4,8 +4,8 @@ namespace MauiApp2.Model
 {
     public class GoalDto : Goal
     {
-        public double Progress { get => CalculateGoalProgress(StartOn, EndOn); set; }
-        private double CalculateGoalProgress(DateTime start, DateTime end)
+        public double Progress { get => CalculateProgress(StartOn, EndOn); set; }
+        public double CalculateProgress(DateTime start, DateTime end)
         {
             var result = 0.0D;
             if (start == null || end == null) return result;
@@ -14,7 +14,7 @@ namespace MauiApp2.Model
             var todayProgress = DateTime.Now - start;
             var goalTime = end - start;
             result = todayProgress / goalTime;
-            return result;  
+            return result;
         }
     }
 }
---
+++ MauiApp2Tests/GoalDtoTests.cs
@@ -0,0 +1,18 @@
+﻿using MauiApp2.Model;
+
+namespace MauiApp2Tests
+{
+    public class GoalDtoTests
+    {
+        [Fact]
+        public void CalculateProgress_WhenStartEarlierThanEndGivenThenZero()
+        {
+            // Arrange
+            var start = new DateTime(2026, 1, 5);
+            var end = new DateTime(2026, 1, 1);
+            var sut = new GoalDto();
+            // Act
+            // Assert
+        }
+    }
+}
