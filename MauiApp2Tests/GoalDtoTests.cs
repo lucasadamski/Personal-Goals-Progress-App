8299271be9811934ddacb8b533125962e4c375c4
Lucas A <lucasadams@protonmail.com>
2026-04-15 16:25:08 +0100

    Implemented logic and solved all tests

--- MauiApp2Tests/GoalDtoTests.cs
+++ MauiApp2Tests/GoalDtoTests.cs
@@ -5,57 +5,99 @@ namespace MauiApp2Tests
 {
     public class GoalDtoTests
     {
+
         [Fact]
-        public void CalculateProgress_WhenStartEarlierThanEndGivenThenZero()
+        public void WhenMethodCalled_GivenStartLaterThanEnd_ThenReturnsZero()
         {
             // Arrange
             var start = new DateTime(2026, 1, 5);
             var end = new DateTime(2026, 1, 1);
+            var today = new DateTime(2026, 1, 5);
+
             var sut = new GoalDto();
             var expectedResult = 0.0F;
             // Act
-            var actualResult = sut.CalculateProgress(start, end);
+            var actualResult = sut.CalculateProgress(start, end, today);
             // Assert
-            actualResult.Should().Be(expectedResult, "because start is before end");
+            actualResult.Should().Be(expectedResult);
         }
         [Fact]
-        public void CalculateProgress_WhenStartSameAsEndGivenThenOne()
+        public void WhenMethodCalled_GivenStartSameAsEndTodaySameDay_ThenReturnsZero()
         {
             // Arrange
             var start = new DateTime(2026, 1, 5);
             var end = new DateTime(2026, 1, 5);
+            var today = new DateTime(2026, 1, 5);
+
+            var sut = new GoalDto();
+            var expectedResult = 0.0F;
+            // Act
+            var actualResult = sut.CalculateProgress(start, end, today);
+            // Assert
+            actualResult.Should().Be(expectedResult);
+        }
+        [Fact]
+        public void WhenMethodCalled_GivenStartSameAsEndTodayTomorrow_ThenReturnsOne()
+        {
+            // Arrange
+            var start = new DateTime(2026, 1, 5);
+            var end = new DateTime(2026, 1, 5);
+            var today = new DateTime(2026, 1, 6);
+
             var sut = new GoalDto();
             var expectedResult = 1.0F;
             // Act
-            var actualResult = sut.CalculateProgress(start, end);
+            var actualResult = sut.CalculateProgress(start, end, today);
             // Assert
-            actualResult.Should().Be(expectedResult, "because start is same day as end");
+            actualResult.Should().Be(expectedResult);
         }
         [Fact]
-        public void GivenMethodCalled_WhenEndNextDay_ThenProgress50Percent()
+        public void WhenMethodCalled_GivenEndNextDay_ThenReturnsProgress0Percent()
         {
             // Arrange
             var start = new DateTime(2026, 1, 5);
             var end = new DateTime(2026, 1, 6);
+            var today = new DateTime(2026, 1, 5);
+
             var sut = new GoalDto();
-            var expectedResult = 0.5F;
+            var expectedResult = 0.0F;
             // Act
-            var actualResult = sut.CalculateProgress(start, end);
+            var actualResult = sut.CalculateProgress(start, end, today);
             // Assert
-            actualResult.Should().Be(expectedResult, "because start is next day");
+            actualResult.Should().Be(expectedResult);
         }
         [Fact]
-        public void GivenMethodCalled_WhenEndInTenDays_ThenProgress10Percent()
+        public void WhenMethodCalled_GivenEndInTenDays_ThenReturnsProgress10Percent()
         {
             // Arrange
             var start = new DateTime(2026, 1, 5);
             var end = new DateTime(2026, 1, 15);
+            var today = new DateTime(2026, 1, 6);
+
+
             var sut = new GoalDto();
-            var expectedResult = 0.1F;
+            var expectedResult = 0.1D;
             // Act
-            var actualResult = sut.CalculateProgress(start, end);
+            var actualResult = sut.CalculateProgress(start, end, today);
             // Assert
-            actualResult.Should().Be(expectedResult, "because end is in 10 days");
+            Math.Round(actualResult, 1).Should().Be(expectedResult);
+        }
+
+        [Fact]
+        public void WhenMethodCalled_GivenEndTomorrowAndStartedNineDaysAgo_ThenReturnsProgress90Percent()
+        {
+            // Arrange
+            var start = new DateTime(2026, 1, 5);
+            var end = new DateTime(2026, 1, 15);
+            var today = new DateTime(2026, 1, 14);
+
+
+            var sut = new GoalDto();
+            var expectedResult = 0.9D;
+            // Act
+            var actualResult = sut.CalculateProgress(start, end, today);
+            // Assert
+            Math.Round(actualResult, 1).Should().Be(expectedResult);
         }
 
     }
